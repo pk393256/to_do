14d4c37b7034753961688ae0b866093c5c77752a
pk393256 <pk393256@gmail.com>
2022-05-28 22:17:30 +0530

    first_part

--- code.js
+++ code.js
@@ -1,41 +1,12 @@
-// let title=document.querySelector('#input1').value;
+
 
 const btn = document.querySelector('#add');
 const output = document.querySelector('#container2');
-
-
-// console.log(tit)
-// const radioButtons = document.querySelectorAll('input[name="check"]');
-// btn.addEventListener('click', () => {
-//     let selecteOption;
-// for (const radioButton of radioButtons) {
-//   if (radioButton.checked) {
-//     selecteOption = radioButton.value;
-//     break;
-//   }
-// }
-// const tit=document.querySelector('#input1').value;
-// console.log(tit)
-// console.log(selecteOption)
-// let newP=document.createElement('h3');
-// newP.innerText=tit;
-// console.log(newP)
-// if(selecteOption==='true'){
-//     newP.style.color='green';
-// }else{
-//     newP.style.color='red';
-// }
-// console.log(newP)
-// output.append(newP);
-// });
-
-// let input=document.getElementById('input1').value;
-// console.log(input)
 let url = 'http://localhost:3000/task'
 btn.addEventListener('click', myFun);
-output.innerHTML='';
+
 async function myFun() {
-    event.preventDefault();
+    // event.preventDefault();
     let input = document.querySelector('#input1').value;
     const radioButtons = document.querySelectorAll('input[name="check"]');
     let selecteOption;
@@ -56,33 +27,43 @@ async function myFun() {
             "Content-type": "application/json; charset=UTF-8"
         }
     })
-    
-    displayData();
-}
-async function displayData(){
-    try {
-        
-        
-        let res=await fetch(url);
-        let data= await res.json();
-        
 
-        data.forEach(e=>{
-            let h3=document.createElement('h3');
-            h3.innerText=e.title;
-            if(e.status=='true'){
-                h3.style.color='green';
-            }else{
-                h3.style.color='red';
+
+}
+displayData();
+async function displayData() {
+    try {
+
+        output.innerHtml = '';
+        let res = await fetch(url);
+        let data = await res.json();
+
+
+        data.forEach(e => {
+            let div = document.createElement('div');
+            let h3 = document.createElement('h3');
+            h3.innerText = e.title;
+            if (e.status == 'true') {
+                h3.style.color = 'green';
+            } else {
+                h3.style.color = 'red';
             }
-            h3.style.borderColor="black";
-            output.append(h3);
+            h3.style.borderColor = "black";
+            let button = document.createElement('button');
+            button.innerText = 'Edit';
+            div.append(h3, button);
+            div.style.display = 'flex';
+            button.style.height='30px';
+            button.style.height='50px';
+            // button.style.left='30px';
+            output.append(div);
+
         })
 
-        
+
 
     } catch (error) {
         console.log(error);
     }
-   
+
 }
